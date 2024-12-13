57cc568082136e24949bf92ce7346a511c129a59
Fellipe Nascimento Guedes <fngprogrammer@hotmail.com>
2024-12-12 22:49:41 -0300

    feat: cria tela home basica

---
+++ timer/src/pages/Home/index.tsx
@@ -0,0 +1,44 @@
+import { Play } from "@phosphor-icons/react";
+import {
+	CountDownContainer,
+	FormContainer,
+	HomeContainer,
+	MinutesAmountInput,
+	Separator,
+	StartCountDownButton,
+	TaskInput,
+} from "./styles";
+
+export default function Home() {
+	return (
+		<HomeContainer>
+			<form>
+				<FormContainer>
+					<label htmlFor="task">Vou trabalhar em</label>
+					<TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
+
+					<label htmlFor="minutesAmount">durante</label>
+					<MinutesAmountInput
+						id="minutesAmount"
+						type="number"
+						placeholder="00"
+					/>
+
+					<span>minutos.</span>
+				</FormContainer>
+				<CountDownContainer>
+					<span>0</span>
+					<span>0</span>
+					<Separator>:</Separator>
+					<span>0</span>
+					<span>0</span>
+				</CountDownContainer>
+
+				<StartCountDownButton type="submit">
+					<Play size={24} />
+					Começar
+				</StartCountDownButton>
+			</form>
+		</HomeContainer>
+	);
+}
